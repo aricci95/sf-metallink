b93d111027d03422b7a0f14df7419ca6b41df7f0
Antoine Ricci <aricci@molotov.tv>
2018-01-05 16:29:37 +0100

    Revert "Init Crop bundle"
    This reverts commit fae606075efb387c05bd9ef3e2b72113e9769027.

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
